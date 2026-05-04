edb98bdcfea3836982801f72aab272db7de8306e
Everton Azevedo <santosazevedoeverton0@gmail.com>
2026-05-04 12:41:53 -0300

    feat:adiciona UsuarioBO

--- src/main/java/br/edu/ifg/luziania/model/bo/UsuarioBO.java
+++ src/main/java/br/edu/ifg/luziania/model/bo/UsuarioBO.java
@@ -1,4 +1,43 @@
 package br.edu.ifg.luziania.model.bo;
 
+import br.edu.ifg.luziania.model.dao.UsuarioDAO;
+import br.edu.ifg.luziania.model.dto.LoginRequestDTO;
+import br.edu.ifg.luziania.model.dto.LoginResponseDTO;
+import br.edu.ifg.luziania.model.entity.Usuario;
+import io.quarkus.elytron.security.common.BcryptUtil;
+import io.smallrye.jwt.build.Jwt;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.WebApplicationException;
+
+@ApplicationScoped
 public class UsuarioBO {
+
+    @Inject
+    UsuarioDAO usuarioDAO;
+
+
+    public LoginResponseDTO realizarLogin(LoginRequestDTO loginRequestDTO) {
+
+        Usuario usuario = usuarioDAO.buscaPorEmail(loginRequestDTO.email());
+        if (usuario == null || !usuario.isAtivo()) {
+            throw new WebApplicationException("login inválido", 401);
+        }
+
+        boolean senha = BcryptUtil.matches(loginRequestDTO.senha(), usuario.getSenha());
+
+        if (!senha) {
+            throw new WebApplicationException("login inválido", 401);
+        }
+
+        String token = Jwt.issuer("https://inventario.ifg.br")
+                .upn(usuario.getEmail()) //email do usuário logado
+                .groups(usuario.getPerfil().name()) // Perfil do usuário
+                .expiresIn(3600) // 1 hora em segundos
+                .sign(); //Assina digitalmente e gera a string final
+
+        return new LoginResponseDTO(token, usuario.getNome(), usuario.getPerfil().name());
+
+    }
 }
