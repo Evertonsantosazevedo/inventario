5ee557fc9a5c24671f7bff5b2d0cabafdfc94ae8
Everton Azevedo <santosazevedoeverton0@gmail.com>
2026-05-05 08:08:14 -0300

    refactor: Corrige método de busca por emai

--- src/main/java/br/edu/ifg/luziania/model/dao/UsuarioDAO.java
+++ src/main/java/br/edu/ifg/luziania/model/dao/UsuarioDAO.java
@@ -1,18 +1,24 @@
 package br.edu.ifg.luziania.model.dao;
 
 import br.edu.ifg.luziania.model.entity.Usuario;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 
-@ApplicationScoped
-public class UsuarioDAO implements PanacheRepository<Usuario> {
+@RequestScoped
+public class UsuarioDAO {
 
-    /***
-     *
-     * @param email
-     * @return primeira ocorrência do param
-     */
-    public Usuario buscaPorEmail(String email){
-        return find("email", email).firstResult();
+    @Inject
+    EntityManager entityManager;
+
+
+    public Usuario buscarPorEmail(String email){
+        //language=jpql
+        String hql = "SELECT u FROM Usuario u WHERE u.email = :email";
+        return (Usuario) entityManager
+                .createQuery(hql)
+                .setParameter("email", email)
+                .getResultStream().findFirst() //Retorna primeira opção encontrada ou null
+                .orElse(null);
     }
 }
