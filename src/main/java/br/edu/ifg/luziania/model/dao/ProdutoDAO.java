c17435f3f7c68f0ccc440f6d73cc29e9bf51f47d
Everton Azevedo <santosazevedoeverton0@gmail.com>
2026-05-08 14:16:59 -0300

    feat: Adiciona buscarPorCodigo

--- src/main/java/br/edu/ifg/luziania/model/dao/ProdutoDAO.java
+++ src/main/java/br/edu/ifg/luziania/model/dao/ProdutoDAO.java
@@ -1,4 +1,23 @@
 package br.edu.ifg.luziania.model.dao;
 
+import br.edu.ifg.luziania.model.entity.Produto;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+
+@RequestScoped
 public class ProdutoDAO {
+
+    @Inject
+    EntityManager entityManager;
+
+    public Produto buscarPorCodigo(String codigo){
+        //language=jpql
+        String hql = "SELECT p FROM Produto p where p.codigo = :codigo";
+        return (Produto) entityManager
+                .createQuery(hql)
+                .setParameter("codigo", codigo)
+                .getResultStream().findFirst()
+                .orElse(null);
+    }
 }
